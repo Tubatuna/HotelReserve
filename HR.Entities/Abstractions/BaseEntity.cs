1f8711c772d774b3936f8dd03967047b4ccdb8dc
Tuğba Tuna <tubaatuna96@gmail.com>
2024-07-20 12:44:07 +0300

    BaseEntity oluşturuldu.

---
+++ HR.Entities/Abstractions/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Entities.Abstractions
+{
+    public interface BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }  
+    }
+}
