44720b9d32d88c6f1675b48bd4f4b5f1e5ae6444
Tuğba Tuna <tubaatuna96@gmail.com>
2024-07-23 22:32:43 +0300

    Formda değişiklik yapıldı.

--- HotelReserve/Form1.Designer.cs
+++ HotelReserve/Form1.Designer.cs
@@ -62,20 +62,17 @@
             // groupBox1
             // 
             groupBox1.BackColor = Color.MistyRose;
-            groupBox1.Controls.Add(dateTimePicker1);
-            groupBox1.Controls.Add(txttel);
-            groupBox1.Controls.Add(txtmail);
-            groupBox1.Controls.Add(txtadres);
-            groupBox1.Controls.Add(txtsoyad);
-            groupBox1.Controls.Add(txtad);
-            groupBox1.Controls.Add(button2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(cmbpaymentmethod);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(datecikis);
+            groupBox1.Controls.Add(dtpgiris);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(cmboda);
             groupBox1.Controls.Add(cmbotel);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(dtpcikis);
             groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
             groupBox1.Location = new Point(23, 23);
             groupBox1.Name = "groupBox1";
@@ -86,50 +83,51 @@
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(271, 384);
+            dateTimePicker1.Location = new Point(192, 332);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(293, 34);
+            dateTimePicker1.Size = new Size(293, 31);
             dateTimePicker1.TabIndex = 11;
             // 
             // txttel
             // 
-            txttel.Location = new Point(271, 287);
+            txttel.Location = new Point(192, 286);
             txttel.Name = "txttel";
-            txttel.Size = new Size(293, 34);
+            txttel.Size = new Size(293, 31);
             txttel.TabIndex = 9;
             // 
             // txtmail
             // 
-            txtmail.Location = new Point(271, 245);
+            txtmail.Location = new Point(192, 249);
             txtmail.Name = "txtmail";
-            txtmail.Size = new Size(293, 34);
+            txtmail.Size = new Size(293, 31);
             txtmail.TabIndex = 8;
             // 
             // txtadres
             // 
-            txtadres.Location = new Point(271, 195);
+            txtadres.Location = new Point(192, 201);
             txtadres.Name = "txtadres";
-            txtadres.Size = new Size(293, 34);
+            txtadres.Size = new Size(293, 31);
             txtadres.TabIndex = 7;
             // 
             // txtsoyad
             // 
-            txtsoyad.Location = new Point(271, 145);
+            txtsoyad.Location = new Point(192, 148);
             txtsoyad.Name = "txtsoyad";
-            txtsoyad.Size = new Size(293, 34);
+            txtsoyad.Size = new Size(293, 31);
             txtsoyad.TabIndex = 6;
             // 
             // txtad
             // 
-            txtad.Location = new Point(271, 97);
+            txtad.Location = new Point(192, 86);
             txtad.Name = "txtad";
-            txtad.Size = new Size(293, 34);
+            txtad.Size = new Size(293, 31);
             txtad.TabIndex = 5;
             // 
             // button2
             // 
+            button2.BackColor = Color.Gainsboro;
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            button2.Location = new Point(271, 459);
+            button2.Location = new Point(192, 482);
             button2.Name = "button2";
             button2.Size = new Size(293, 56);
             button2.TabIndex = 4;
@@ -141,7 +139,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label1.Location = new Point(6, 389);
+            label1.Location = new Point(6, 335);
             label1.Name = "label1";
             label1.Size = new Size(140, 28);
             label1.TabIndex = 2;
@@ -151,7 +149,7 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label6.Location = new Point(19, 293);
+            label6.Location = new Point(6, 307);
             label6.Name = "label6";
             label6.Size = new Size(82, 28);
             label6.TabIndex = 2;
@@ -161,7 +159,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label5.Location = new Point(19, 252);
+            label5.Location = new Point(6, 252);
             label5.Name = "label5";
             label5.Size = new Size(82, 28);
             label5.TabIndex = 2;
@@ -171,7 +169,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label4.Location = new Point(19, 201);
+            label4.Location = new Point(6, 204);
             label4.Name = "label4";
             label4.Size = new Size(66, 28);
             label4.TabIndex = 2;
@@ -181,7 +179,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label3.Location = new Point(19, 152);
+            label3.Location = new Point(1, 153);
             label3.Name = "label3";
             label3.Size = new Size(69, 28);
             label3.TabIndex = 2;
@@ -192,7 +190,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label2.Location = new Point(19, 104);
+            label2.Location = new Point(18, 95);
             label2.Name = "label2";
             label2.Size = new Size(38, 28);
             label2.TabIndex = 2;
@@ -210,16 +208,16 @@
             // cmboda
             // 
             cmboda.FormattingEnabled = true;
-            cmboda.Location = new Point(170, 41);
+            cmboda.Location = new Point(271, 105);
             cmboda.Name = "cmboda";
-            cmboda.Size = new Size(217, 33);
+            cmboda.Size = new Size(293, 36);
             cmboda.TabIndex = 11;
             // 
             // label8
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label8.Location = new Point(18, 44);
+            label8.Location = new Point(19, 113);
             label8.Name = "label8";
             label8.Size = new Size(92, 28);
             label8.TabIndex = 2;
@@ -228,21 +226,24 @@
             // groupBox2
             // 
             groupBox2.BackColor = Color.MistyRose;
-            groupBox2.Controls.Add(cmbpaymentmethod);
-            groupBox2.Controls.Add(label12);
-            groupBox2.Controls.Add(datecikis);
-            groupBox2.Controls.Add(dtpgiris);
-            groupBox2.Controls.Add(button1);
-            groupBox2.Controls.Add(cmboda);
-            groupBox2.Controls.Add(label10);
-            groupBox2.Controls.Add(dtpcikis);
-            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(dateTimePicker1);
+            groupBox2.Controls.Add(txtad);
+            groupBox2.Controls.Add(txtsoyad);
+            groupBox2.Controls.Add(txttel);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(txtmail);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(txtadres);
+            groupBox2.Controls.Add(label5);
             groupBox2.Controls.Add(label9);
-            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label3);
             groupBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
             groupBox2.Location = new Point(651, 23);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(446, 569);
+            groupBox2.Size = new Size(505, 569);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "Rezervasyon Formu";
@@ -250,9 +251,9 @@
             // cmbpaymentmethod
             // 
             cmbpaymentmethod.FormattingEnabled = true;
-            cmbpaymentmethod.Location = new Point(170, 224);
+            cmbpaymentmethod.Location = new Point(271, 332);
             cmbpaymentmethod.Name = "cmbpaymentmethod";
-            cmbpaymentmethod.Size = new Size(217, 33);
+            cmbpaymentmethod.Size = new Size(293, 36);
             cmbpaymentmethod.TabIndex = 17;
             cmbpaymentmethod.SelectedIndexChanged += cmbpaymentmethod_SelectedIndexChanged;
             // 
@@ -260,7 +261,7 @@
             // 
             label12.AutoSize = true;
             label12.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label12.Location = new Point(18, 154);
+            label12.Location = new Point(19, 252);
             label12.Name = "label12";
             label12.Size = new Size(103, 25);
             label12.TabIndex = 16;
@@ -268,21 +269,21 @@
             // 
             // datecikis
             // 
-            datecikis.Location = new Point(170, 149);
+            datecikis.Location = new Point(271, 244);
             datecikis.Name = "datecikis";
-            datecikis.Size = new Size(217, 31);
+            datecikis.Size = new Size(293, 34);
             datecikis.TabIndex = 15;
             // 
             // dtpgiris
             // 
-            dtpgiris.Location = new Point(170, 99);
+            dtpgiris.Location = new Point(271, 172);
             dtpgiris.Name = "dtpgiris";
-            dtpgiris.Size = new Size(217, 31);
+            dtpgiris.Size = new Size(293, 34);
             dtpgiris.TabIndex = 14;
             // 
             // button1
             // 
-            button1.Location = new Point(170, 331);
+            button1.Location = new Point(334, 474);
             button1.Name = "button1";
             button1.Size = new Size(230, 64);
             button1.TabIndex = 13;
@@ -294,7 +295,7 @@
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label10.Location = new Point(6, 427);
+            label10.Location = new Point(408, 411);
             label10.Name = "label10";
             label10.Size = new Size(156, 28);
             label10.TabIndex = 2;
@@ -304,7 +305,7 @@
             // 
             dtpcikis.AutoSize = true;
             dtpcikis.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            dtpcikis.Location = new Point(18, 224);
+            dtpcikis.Location = new Point(12, 332);
             dtpcikis.Name = "dtpcikis";
             dtpcikis.Size = new Size(121, 28);
             dtpcikis.TabIndex = 2;
@@ -314,7 +315,7 @@
             // 
             label11.AutoSize = true;
             label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label11.Location = new Point(18, 101);
+            label11.Location = new Point(19, 178);
             label11.Name = "label11";
             label11.Size = new Size(114, 28);
             label11.TabIndex = 2;
